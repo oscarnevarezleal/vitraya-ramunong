065b0a8a61b9369a628515a1a621afc97824c418
Oscar Nevarez <fu.wire@gmail.com>
2021-11-19 09:12:08 -0600

    Squash progress (#1)
    * chore: wip: gh api forks
    
    * chore: wip: gh api forks
    
    * chore: wip: gh api forks
    
    * chore: wip: gh api forks
    
    * chore: wip: gh api forks
    
    * chore: wip: gh api forks
    
    * chore: wip: gh api forks
    
    * chore: wip: gh api forks
    
    * chore: wip: gh api forks
    
    * chore: wip: gh api forks
    
    * chore: wip: gh api forks
    
    * chore: wip: gh api forks
    
    * chore: wip: gh api forks
    
    * chore(forks): regenerate forks.md
    
    * chore: wip: gh api forks
    
    * chore(forks): regenerate forks.md
    
    * chore: reset FORKS.md
    
    * chore(forks): regenerate forks.md
    
    * chore: action integration
    
    * chore(forks): regenerate forks.md
    
    * chore: action integration
    
    * chore(forks): regenerate forks.md
    
    * chore: action integration
    
    * chore(forks): regenerate forks.md
    
    Co-authored-by: oscarnevarezleal <oscarnevarezleal@users.noreply.github.com>

---
+++ scripts/gen-forks-file.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo "# Forks" > FORKS.md
+echo "## Here's the list of forks" >>FORKS.md
+for row in $(echo "${REPO_FORKS}" | jq -r '.[] | @base64'); do
+  _jq() {
+    echo ${row} | base64 --decode | jq -r ${1}
+  }
+  login=$(_jq '.owner.login')
+  node=$(curl -s https://api.github.com/repos/${OWNER}/${REPO}/compare/main...${login}:main |
+    jq 'with_entries(if (.key|test("(ahead|behind)")) then ( {key: .key, value: .value } ) else empty end )')
+
+  _jqNode() {
+    echo ${node} | jq -r ${1}
+  }
+
+  echo "- [$login](https:/$login}/${REPO}) is $(_jqNode '.ahead_by') commit(s) ahead and, $(_jqNode '.ahead_by') commit(s) behind" >>FORKS.md
+  echo " " >>FORKS.md
+done
+
+cat FORKS.md
